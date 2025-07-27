bbf8ee201621fcea39b6ab3b12778933b8ddf116
Vijaykumar Shinde <138961173+vijayns2157@users.noreply.github.com>
2025-07-27 17:55:17 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,5 @@ docker pull vijayshinde2157/simple-python-new-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vijayshinde2157/simple-python-new-app
+
+echo "Everything is successful"
